0f54e5bfa49ac196dcd3373655824ee3256000ba
Volkan Akkan <akkan.volkan@gmail.com>
2025-07-22 19:34:06 +0200

    MavenTest 2.1.1

--- src/test/java/enums/DaysTest.java
+++ src/test/java/enums/DaysTest.java
@@ -15,6 +15,46 @@ public class DaysTest {
     }
 
     @Test
+    @DisplayName("TUESDAY Enum-Constant should have proper words in german, english and turkish")
+    void tuesday_ShouldHaveCorrectNames() {
+        assertEquals("Dienstag", Days.TUESDAY.getDeutscheBezeichnung(), "TUESDAY should have 'Dienstag' as german word.");
+        assertEquals("Tuesday", Days.TUESDAY.getEnglishName(), "TUESDAY should have 'Tuesday' as english word.");
+        assertEquals("Salı", Days.TUESDAY.getTurkishName(), "TUESDAY should have 'Salı' as turkish word.");
+    }
+
+    @Test
+    @DisplayName("WEDNESDAY Enum-Constant should have proper words in german, english and turkish")
+    void wednesday_ShouldHaveCorrectNames() {
+        assertEquals("Mittwoch", Days.WEDNESDAY.getDeutscheBezeichnung(), "WEDNESDAY should have 'Mittwoch' as german word.");
+        assertEquals("Wednesday", Days.WEDNESDAY.getEnglishName(), "WEDNESDAY should have 'Wednesday' as english word.");
+        assertEquals("Çarşamba", Days.WEDNESDAY.getTurkishName(), "WEDNESDAY should have 'Çarşamba' as turkish word.");
+    }
+
+    @Test
+    @DisplayName("THURSDAY Enum-Constant should have proper words in german, english and turkish")
+    void thursday_ShouldHaveCorrectNames() {
+        assertEquals("Donnerstag", Days.THURSDAY.getDeutscheBezeichnung(), "THURSDAY should have 'Donnerstag' as german word.");
+        assertEquals("Thursday", Days.THURSDAY.getEnglishName(), "THURSDAY should have 'Thursday' as english word.");
+        assertEquals("Perşembe", Days.THURSDAY.getTurkishName(), "THURSDAY should have 'Perşembe' as turkish word.");
+    }
+
+    @Test
+    @DisplayName("FRIDAY Enum-Constant should have proper words in german, english and turkish")
+    void friday_ShouldHaveCorrectNames() {
+        assertEquals("Freitag", Days.FRIDAY.getDeutscheBezeichnung(), "FRIDAY should have 'Freitag' as german word.");
+        assertEquals("Friday", Days.FRIDAY.getEnglishName(), "FRIDAY should have 'Friday' as english word.");
+        assertEquals("Cuma", Days.FRIDAY.getTurkishName(), "FRIDAY should have 'Cuma' as turkish word.");
+    }
+
+    @Test
+    @DisplayName("SATURDAY Enum-Constant should have proper words in german, english and turkish")
+    void saturday_ShouldHaveCorrectNames() {
+        assertEquals("Samstag", Days.SATURDAY.getDeutscheBezeichnung(), "SATURDAY should have 'Samstag' as german word.");
+        assertEquals("Saturday", Days.SATURDAY.getEnglishName(), "SATURDAY should have 'Saturday' as english word.");
+        assertEquals("Cumartesi", Days.SATURDAY.getTurkishName(), "SATURDAY should have 'Cumartesi' as turkish word.");
+    }
+
+    @Test
     @DisplayName("SUNDAY Enum-Constant should have proper words in german, english and turkish")
     void sunday_ShouldHaveCorrectNames() {
         assertEquals("Sonntag", Days.SUNDAY.getDeutscheBezeichnung(), "SUNDAY should have 'Sonntag' as german word.");
